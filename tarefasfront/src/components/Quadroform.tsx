62bd672fff192594c37003073d9234f08d600004
Pedrocardo005 <pedrocardo007@hotmail.com>
2024-02-22 21:26:55 -0300

    Add componente de botao e textarea

--- tarefasfront/src/components/Quadroform.tsx
+++ tarefasfront/src/components/Quadroform.tsx
@@ -1,12 +1,24 @@
-import { ChangeEvent, useState } from "react";
+import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 export function QuadroForm() {
     return (
-        <div className="flex">
-            <Input name="titulo" type="text" placeholder="Título" />
-            <Input name="dia-semana" type="date" />
-            <Input name="hora" type="time" min={"08:00"} max={"22:00"} />
+        <div className="flex flex-row-reverse">
+            <div className="flex w-10/12 justify-end">
+                <form className="flex w-full gap-4">
+                    <div className="w-full">
+                        <Input name="titulo" type="text" placeholder="Título" />
+                        <Textarea />
+                    </div>
+                    
+                    <Input name="dia-semana" type="date" className="w-full" />
+                    <Input name="hora" type="time" min={"08:00"} max={"22:00"} className="w-full" />
+                    <Button className="mr-3">
+                        Enviar
+                    </Button>
+                </form>
+            </div>
         </div>
     );
 }
